846947bcab57e300970db07a9d58ae7ae39023e3
Bernardo A. Dal Corno <bernardoadc@gmail.com>
2021-03-24 04:32:38 -0300

    basic code

---
+++ plugins/parser/default.config.js
@@ -0,0 +1,13 @@
+export default {
+  indentSize: 2,
+  insertSpaces: true,
+  'comments.inline': '//',
+  'comments.multilines.open': '/*',
+  'comments.multilines.close': '*/',
+  id: '[]',
+  'tasks.unordered': '*',
+  'tasks.ordered': ')',
+  customtypes: {
+    objetivos: '->'
+  }
+}
